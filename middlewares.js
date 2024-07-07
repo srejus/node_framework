d4e4237eac5d43fc1376f980d1c9adde29fd1c46
srejus <srejusjayakumar@gmail.com>
2024-07-07 19:26:38 +0530

    file handiling added

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const multer = require('multer');
+const path = require('path');
 
 const jwt = require('jsonwebtoken');
 
@@ -21,4 +23,18 @@ const validateToken = async(req,res,next) => {
     }
 }
 
-module.exports = validateToken
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      const baseName = path.basename(file.originalname, ext);
+      cb(null, `${baseName}-${Date.now()}${ext}`);
+    }
+  });
+  
+const upload = multer({ storage: storage });
+
+module.exports = {validateToken,upload}
